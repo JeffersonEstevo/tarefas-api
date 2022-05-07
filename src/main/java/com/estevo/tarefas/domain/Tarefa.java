d0850d311715b2ac6446adb11512e0cb03d6943f
JeffersonEstevo <jefferson.estevo@gmail.com>
2022-05-06 21:02:45 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/estevo/tarefas/domain/Tarefa.java
@@ -0,0 +1,105 @@
+package com.estevo.tarefas.domain;
+
+import java.util.Objects;
+
+public class Tarefa {
+	private Integer id;
+	private String titulo;
+	private String descricao;
+	private String responsavel;
+	private String prioridade;
+	private String data;
+
+	private Categoria categoria;
+
+	public Tarefa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tarefa(Integer id, String titulo, String descricao, String responsavel, String prioridade, String data,
+			Categoria categoria) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.responsavel = responsavel;
+		this.prioridade = prioridade;
+		this.data = data;
+		this.categoria = categoria;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+
+	public String getPrioridade() {
+		return prioridade;
+	}
+
+	public void setPrioridade(String prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarefa other = (Tarefa) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
